b7a4f56019573e402270ecbe35b3542ad057cd00
Abdulakfi88 <abdulkafinirig5@gmail.com>
2023-11-05 01:18:02 -0800

    now you can clikc on the header icons

--- TOdoList/src/com/Header.jsx
+++ TOdoList/src/com/Header.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import Logo from './img/logo.jpg'
 const Header = () => {
+ const [theme,setTheme] = useState('theme')
+
   return (
     <header>
       <div className="logo">
@@ -7,14 +10,16 @@ const Header = () => {
         <span></span>
       </div>
       <div className='themeSelector'>
-        <span className='light'></span>
-        <span className='medium'></span>
-        <span className='dark'></span>
-        <span className='gradientOne'></span>
-        <span className='gradientTwo'></span>
-        <span className='gradientThree activeTheme'></span>
-      
+      <div className="themeSelector">
+            <span onClick={() => setTheme("light")} className={ theme === "light" ? "light activeTheme" : "light"}></span>
+            <span onClick={() => setTheme("medium")} className={ theme === "medium" ? "medium activeTheme" : "medium"}></span>
+            <span onClick={() => setTheme("dark")} className={ theme === "dark" ? "dark activeTheme" : "dark"}></span>
+            <span onClick={() => setTheme("gradientOne")} className={ theme === "gradientOne" ? "gradientOne activeTheme" : "gradientOne"}></span>
+            <span onClick={() => setTheme("gradientTwo")} className={ theme === "gradientTwo" ? "gradientTwo activeTheme" : "gradientTwo"}></span>
+            <span onClick={() => setTheme("gradientThree")} className={ theme === "gThree" ? "gradientThree activeTheme" : "gradientThree"}></span>
+        </div>
       </div>
+      
     </header>
   );
 };
